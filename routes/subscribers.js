d184d6e643351bf6249b2f19e47806f8985d1584
HenrikCheng <henrikcheng@live.se>
2023-06-07 14:00:12 +0200

    add middleware to find each sub

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const Subscriber = require("../models/subscriber");
+const subscriber = require("../models/subscriber");
 
 // Getting all
 router.get("/", async (req, res) => {
-  // res.send("get all");
   try {
     const subscribers = await Subscriber.find();
     res.json(subscribers);
@@ -14,23 +14,48 @@ router.get("/", async (req, res) => {
 });
 
 // Getting one
-router.get("/:id", (req, res) => {
-  res.send(req.params.id);
+router.get("/:id", getSubscriber, (req, res) => {
+  res.send(res.subscriber.name);
 });
 
 // Creating one
-router.post("/", (req, res) => {
-  res.send("create one");
+router.post("/", async (req, res) => {
+  const subscriber = new Subscriber({
+    name: req.body.name,
+    subscribedToChannel: req.body.subscribedToChannel,
+  });
+
+  try {
+    const newSubscriber = await subscriber.save();
+    res.status(201).json(newSubscriber); // 201 means successfully created
+  } catch (error) {
+    res.status(400).json({ message: error.message }); // 400 means something wrong with user input
+  }
 });
 
 // Updating one
-router.patch("/:id", (req, res) => {
+router.patch("/:id", getSubscriber, (req, res) => {
   res.send(req.params.id);
 });
 
 // Deleting one
-router.delete("/:id", (req, res) => {
+router.delete("/:id", getSubscriber, (req, res) => {
   res.send("delete one");
 });
+
+async function getSubscriber(req, res, next) {
+  let subscriber;
+  try {
+    subscriber = await Subscriber.findById(req.params.id);
+    if (subscriber === null) {
+      return res.status(404).json({ message: "Cannot find subscriber" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+
+  res.subscriber = subscriber;
+  next();
+}
 
 module.exports = router;
